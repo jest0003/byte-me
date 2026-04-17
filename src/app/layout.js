7b581d0f661297b46f071a32602ca3a7f6010e05
Jerie <jerie.stolbergrohr@gmail.com>
2026-04-17 14:54:22 +0200

    layout opstart

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Exo_2, Gidole} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const exo2 = Exo_2({
+  variable: "--font-exo2",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const gidole = Gidole({
+  variable: "--font-gidole",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -20,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${exo2.variable} ${gidole.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
