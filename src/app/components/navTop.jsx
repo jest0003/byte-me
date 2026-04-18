9d303e5fd9d51413ce474414e1ecb552bd92375e
Jerie <jerie.stolbergrohr@gmail.com>
2026-04-18 11:33:45 +0200

    almost done

--- src/app/components/navTop.jsx
+++ src/app/components/navTop.jsx
@@ -6,21 +6,21 @@ import { RiShoppingBasket2Line } from "react-icons/ri";
 
 const navTop = () => {
     return (
-        <nav className="flex items-center justify-between mt-2">
-            <div>
-<SiAirbyte />
+        <nav className="flex items-center justify-between mt-3 mb-12">
+            <div className="flex items-center">
+<SiAirbyte className="size-8 mr-2" /> <h4>ByteMe</h4>
             </div>
-            <div className="flex items-center gap-4">
-                <a href="/"><h5>Home</h5></a>
-                <a href="/"><h5>Phones</h5></a>
-                <a href="/"><h5>Tablets</h5></a>
-                <a href="/"><h5>Laptops</h5></a>
-                <a href="/"><h5>Accessories</h5></a>
+            <div className="flex items-center justify-end gap-4 ml-6">
+                <a href="/"><h5 className="hover:underline">Home</h5></a>
+                <a href="/"><h5 className="hover:underline">Phones</h5></a>
+                <a href="/"><h5 className="hover:underline">Tablets</h5></a>
+                <a href="/"><h5 className="hover:underline">Laptops</h5></a>
+                <a href="/"><h5 className="hover:underline">Accessories</h5></a>
             </div>
-            <div className="flex">
-                <IoMdSearch />
-                <FiHeart />
-                <RiShoppingBasket2Line />
+            <div className="flex items-center">
+                <IoMdSearch className="size-7 mr-3" />
+                <FiHeart className="size-6 mr-3" />
+                <RiShoppingBasket2Line className="size-7" />
             </div>
         </nav>
     );
