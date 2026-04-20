9127a4e22b670323a67c516d212b4c60ce063cd4
Jerie <jerie.stolbergrohr@gmail.com>
2026-04-20 09:42:49 +0200

    nprmal footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 	/* 151318 */
 	return (
-		<nav className="bg-[#6d8273] text-white px-5 py-10 fixed bottom-0 left-0 w-full">
+		<nav className=" text-white px-5 py-10 fixed bottom-0 left-0 w-full">
 			<div className="max-w-[1200px] mx-auto flex justify-between gap-8">
 				<div className="flex flex-1 mt-10">
 					<div className="flex-1">
