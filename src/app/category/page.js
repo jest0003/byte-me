a3a23fc46822adb2234345244a4d5e718c2f1c0d
Jerie <jerie.stolbergrohr@gmail.com>
2026-04-20 12:33:33 +0200

    Filter og pics done

--- src/app/category/page.js
+++ src/app/category/page.js
@@ -1,6 +1,23 @@
+import Filter from "../components/category/Filter";
 const Category = () => {
     return ( 
-        <h1>Works?</h1>
+        <div>
+        <article className="relative pb-16">
+<Filter />
+<img src="/bg-tech/tech-long.png"
+height={100}
+className="w-full h-20 object-cover opacity-30"></img>
+
+<section className="absolute top-14">
+    <p>Bred / crumb </p>
+        <h1>Phones</h1>
+</section>
+        </article>
+        <article>
+            <p>Tekst</p>
+        </article>
+        <img src="/bg-tech/tech-short.png" className="w-full h-40 object-cover opacity-30"></img>
+        </div>
      );
 }
  
