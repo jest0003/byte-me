5399746e71ba45a3c1e4777fa0115c5e5ff214ac
Monika <moha0009@stud.kea.dk>
2026-04-20 09:30:02 +0200

    Singleview

---
+++ src/stores/favorite.jsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useFavorites = create(
+  persist(
+    (set) => ({
+      favorites: [],
+
+      setFavorite: (id) =>
+        set((state) => ({
+          favorites: [...state.favorites, { id }],
+        })),
+
+      removeFavorite: (id) =>
+        set((state) => ({
+          favorites: state.favorites.filter((f) => f.id !== id),
+        })),
+    }),
+    {
+      name: "favorites-storage",
+    },
+  ),
+);
+
+export default useFavorites;
+
+// "use client";
+
+// import { create } from "zustand";
+// import { persist } from "zustand/middleware";
+// const useFavorites = create(
+//   persist(
+//     (set) => ({
+//       ratings: {}, // { [id]: number }
+
+//       setRating: (id, value) =>
+//         set((state) => ({
+//           ratings: {
+//             ...state.ratings,
+//             [id]: value,
+//           },
+//         })),
+
+//       removeRating: (id) =>
+//         set((state) => {
+//           const copy = { ...state.ratings };
+//           delete copy[id];
+//           return { ratings: copy };
+//         }),
+//     }),
+//     {
+//       name: "favorites-storage",
+//     },
+//   ),
+// );
+
+// export default useFavorites;
+
+// "use client";
+
+// import { create } from "zustand";
+// import { persist } from "zustand/middleware";
+
+// const useFavorites = create(
+//   persist(
+//     (set) => ({
+//       favorites: [],
+
+//       setFavorite: (id) =>
+//         set((state) => ({
+//           favorites: [...state.favorites, { id }],
+//         })),
+
+//       removeFavorite: (id) =>
+//         set((state) => ({
+//           favorites: state.favorites.filter((f) => f.id !== id),
+//         })),
+//     }),
+//     {
+//       name: "favorites-storage",
+//     },
+//   ),
+// );
+
+// export default useFavorites;
