70b00b8d90610eb368d143ec18b75d4ca4e8614e
Celia <cmarianielsen@gmail.com>
2026-04-21 09:31:56 +0200

    styling næsten færdig

--- src/app/components/indexpage/CategoryCard.jsx
+++ src/app/components/indexpage/CategoryCard.jsx
@@ -1,6 +1,6 @@
 const CategoryCard = async () => {
 	return (
-		<div /* style={{ background: "var(--color-2)" }} */ className="my-6 rounded-2xl category-card">
+		<div className="my-6 rounded-2xl category-card">
 			<figure className="flex h-[200px] w-[200px] items-center justify-center p-2">
 				<img src="/img/mobil.png" className="" alt="category" />
 			</figure>
