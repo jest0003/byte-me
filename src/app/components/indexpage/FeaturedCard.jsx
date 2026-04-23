230391e8724cac639d7d555210bcbddb195a0fd9
Celia <cmarianielsen@gmail.com>
2026-04-23 09:44:25 +0200

    fixes

--- src/app/components/indexpage/FeaturedCard.jsx
+++ src/app/components/indexpage/FeaturedCard.jsx
@@ -3,22 +3,22 @@ import FavoritElement from "./FavoritElement";
 import Image from "next/image";
 import Link from "next/link";
 
-const FeaturedCard = ({ id, title, price, image }) => {
-  return (
-    <div style={{ background: "var(--color-3)" }} className="my-4 rounded-2xl max-w-full max-h-full w-[275px] h-[330px]">
-      <figure className=" m-5 flex items-center justify-center p-2 relative pt-7">
-        <FavoritElement id={id}></FavoritElement>
-        <Image src={image} alt={title} width={150} height={150} />
-      </figure>
-      <Link href={`/${id}`}>
-        <p className="m-5">{title}</p>
-      </Link>
-      <div className=" m-5 flex justify-between">
-        <p className="">{price}$</p>
-        <RiShoppingBasket2Line className="size-9 cursor-pointer" />
-      </div>
-    </div>
-  );
+const FeaturedCard = ({ id, title, price, image, category }) => {
+	return (
+		<div style={{ background: "var(--color-3)" }} className="my-4 rounded-2xl max-w-full max-h-full w-[275px] h-[330px]">
+			<figure className=" m-5 flex items-center justify-center p-2 relative pt-7">
+				<FavoritElement id={id}></FavoritElement>
+				<Image src={image} alt={title} width={150} height={150} />
+			</figure>
+			<Link href={`category/${category}/${id}`}>
+				<p className="m-5">{title}</p>
+			</Link>
+			<div className=" m-5 flex justify-between">
+				<p className="">{price}$</p>
+				<RiShoppingBasket2Line className="size-9 cursor-pointer" />
+			</div>
+		</div>
+	);
 };
 
 export default FeaturedCard;
